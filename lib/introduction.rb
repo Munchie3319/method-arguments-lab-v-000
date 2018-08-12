718a804f53c9a209043a8e5a4a3265dd75734e55
rosa lee <rosalee0116@gmail.com>
2018-08-12 23:57:26 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_coding(name, coding)
+def introduction_with_language(name, coding)
   puts "Hi, my name is #{name}, and I am learning  #{coding}."
 end
